ac5b7883afb62666d15221e0eba64639379322b4
bitniki <shakarimovdima@ya.ru>
2022-08-16 12:01:11 +0300

    first commit

---
+++ src/main/java/com/bitniki/VPNconServer/VpNconServerApplication.java
@@ -0,0 +1,13 @@
+package com.bitniki.VPNconServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpNconServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpNconServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitniki/VPNconServer/VpNconServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitniki.VPNconServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpNconServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
