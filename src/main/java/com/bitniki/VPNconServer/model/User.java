8437e3bff43a8b517f5910380d8399d6abb36b12
bitniki <shakarimovdima@ya.ru>
2022-08-21 15:50:56 +0300

    add join to peer table

--- src/main/java/com/bitniki/VPNconServer/model/User.java
+++ src/main/java/com/bitniki/VPNconServer/model/User.java
@@ -1,15 +1,21 @@
 package com.bitniki.VPNconServer.model;
 
+import com.bitniki.VPNconServer.entity.PeerEntity;
 import com.bitniki.VPNconServer.entity.UserEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
     private Long id;
     private String login;
+    private List<Peer> peers;
 
     public static User toModel (UserEntity entity) {
         User model = new User();
         model.setId(entity.getId());
         model.setLogin(entity.getLogin());
+        model.setPeers(entity.getPeers());
 
         return model;
     }
@@ -31,5 +37,13 @@ public class User {
 
     public void setLogin(String login) {
         this.login = login;
+    }
+
+    public List<Peer> getPeers() {
+        return peers;
+    }
+
+    public void setPeers(List<PeerEntity> peerEntities) {
+        this.peers = peerEntities.stream().map(Peer::toModel).collect(Collectors.toList());
     }
 }
