7845c399d79d4dc617577e1dcb198e6496e140dd
BITniki <shakarimovdima@ya.ru>
2022-08-30 22:45:29 +0300

    init

---
+++ src/main/java/com/bitniki/VPNconServer/exception/alreadyExistException/PeerAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.bitniki.VPNconServer.exception.alreadyExistException;
+
+public class PeerAlreadyExistException extends Exception{
+    public PeerAlreadyExistException(String message) {
+        super(message);
+    }
+}
