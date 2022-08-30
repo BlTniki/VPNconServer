81b24d9d7b17f7a9b0c5a95e6cfb911680c979fd
BITniki <shakarimovdima@ya.ru>
2022-08-30 23:03:04 +0300

    init

--- src/main/java/com/bitniki/VPNconServer/repository/PeerRepo.java
+++ src/main/java/com/bitniki/VPNconServer/repository/PeerRepo.java
@@ -1,7 +1,9 @@
 package com.bitniki.VPNconServer.repository;
 
 import com.bitniki.VPNconServer.entity.PeerEntity;
+import com.bitniki.VPNconServer.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PeerRepo extends CrudRepository<PeerEntity, Long> {
+    PeerEntity findByUserAndPeerConfName(UserEntity user, String peerConfName);
 }
