b42b870bea7170574d0f4a78c3d488de742c87a3
BITniki <shakarimovdima@ya.ru>
2022-09-15 15:08:03 +0300

    add permission check for endpoints

--- src/main/java/com/bitniki/VPNconServer/controller/HostController.java
+++ src/main/java/com/bitniki/VPNconServer/controller/HostController.java
@@ -9,6 +9,7 @@ import com.bitniki.VPNconServer.model.HostWithRelations;
 import com.bitniki.VPNconServer.service.HostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,28 +21,33 @@ public class HostController {
     private HostService hostService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('host:read')")
     public ResponseEntity<List<HostWithRelations>> getAllHosts() {
         return ResponseEntity.ok(hostService.getAll());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('host:read')")
     public ResponseEntity<HostWithRelations> getHost (@PathVariable Long id)
             throws HostNotFoundException {
         return ResponseEntity.ok(hostService.getOne(id));
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('host:write')")
     public ResponseEntity<Host> createHost(@RequestBody HostEntity host)
             throws HostAlreadyExistException, HostValidationFailedException {
         return ResponseEntity.ok(hostService.create(host));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('host:write')")
     public ResponseEntity<Host> updateHost (@PathVariable Long id, @RequestBody HostEntity host)
             throws HostNotFoundException, HostAlreadyExistException, HostValidationFailedException {
         return ResponseEntity.ok(hostService.update(id, host));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('host:write')")
     public ResponseEntity<Host> deleteHost (@PathVariable Long id) throws HostNotFoundException {
         return ResponseEntity.ok(hostService.delete(id));
     }
