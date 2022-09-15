e6d1ca85880d0562c263de1effbb81b4a43d3b35
BITniki <shakarimovdima@ya.ru>
2022-09-15 10:48:54 +0300

    add password encoding

--- src/main/java/com/bitniki/VPNconServer/service/UserService.java
+++ src/main/java/com/bitniki/VPNconServer/service/UserService.java
@@ -9,6 +9,7 @@ import com.bitniki.VPNconServer.model.UserWithRelations;
 import com.bitniki.VPNconServer.repository.UserRepo;
 import com.bitniki.VPNconServer.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ import java.util.stream.Collectors;
 public class UserService {
     @Autowired
     private UserRepo userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public List<UserWithRelations> getAll() {
         List<UserEntity> users = new ArrayList<>();
@@ -34,18 +37,25 @@ public class UserService {
     }
 
     public User create (UserEntity user) throws UserAlreadyExistException, UserValidationFailedException {
+        // valid entity
         UserValidator userValidator = UserValidator.validateAllFields(user);
         if(userValidator.hasFails()){
             throw new UserValidationFailedException(userValidator.toString());
         }
-
+        // check in repo login unique
         if(userRepo.findByLogin(user.getLogin()) != null) {
             throw new UserAlreadyExistException("That login already taken!");
         }
+
+        // configure entity
+        // encode password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         return User.toModel(userRepo.save(user));
     }
 
     public User update (Long id, UserEntity newUser) throws UserAlreadyExistException, UserNotFoundException, UserValidationFailedException {
+        // valid new entity
         UserValidator userValidator = UserValidator.validateNonNullFields(newUser);
         if(userValidator.hasFails()){
             throw new UserValidationFailedException(userValidator.toString());
@@ -56,16 +66,22 @@ public class UserService {
             throw new UserAlreadyExistException("User with that login already exist!");
         }
 
+        // load old entity
         Optional<UserEntity> oldUserEntityOptional;
         UserEntity oldUser;
         oldUserEntityOptional = userRepo.findById(id);
         if(oldUserEntityOptional.isPresent()) oldUser = oldUserEntityOptional.get();
         else throw new UserNotFoundException("User not found");
 
+        // encode password
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+
+        //update, save and return entity
         return User.toModel(userRepo.save(UserEntity.updateEntity(oldUser,newUser)));
     }
 
     public User delete(Long id) throws UserNotFoundException {
+        // load entity
         Optional<UserEntity> userEntityOptional;
         UserEntity user;
         userEntityOptional = userRepo.findById(id);
