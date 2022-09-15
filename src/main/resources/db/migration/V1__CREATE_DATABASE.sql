560e641cd904e19de13888e3601187589bae8d8e
BITniki <shakarimovdima@ya.ru>
2022-09-15 14:51:02 +0300

    add role column in user table

--- src/main/resources/db/migration/V1__CREATE_DATABASE.sql
+++ src/main/resources/db/migration/V1__CREATE_DATABASE.sql
@@ -7,22 +7,23 @@ insert into hibernate_sequence values ( 1 );
 
 CREATE TABLE user (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    login varchar(64) not null unique,
-    password varchar(255) not null,
+    login varchar(64) NOT NULL UNIQUE,
+    password varchar(255) NOT NULL,
+    role varchar(255) NOT NULL DEFAULT "DISABLED_USER",
     PRIMARY KEY (id)
 ) engine=MyISAM;
 
 CREATE TABLE host (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    ipadress varchar(21) not null unique,
-    server_password varchar(255) not null,
-    server_public_key varchar(255) not null,
+    ipadress varchar(64) NOT NULL UNIQUE,
+    server_password varchar(255) NOT NULL,
+    server_public_key varchar(255) NOT NULL,
     PRIMARY KEY (id)
 ) engine=MyISAM;
 
 CREATE TABLE peer (
     id BIGINT NOT NULL AUTO_INCREMENT,
-    peer_ip varchar(21) not null,
+    peer_ip varchar(21) NOT NULL,
     peer_private_key varchar(255) not null,
     peer_public_key varchar(255) not null,
     peer_conf_name varchar(255) not null,
