46b7820cc78705fabd8fc10b9ef2da22b3b430b7
BITniki <shakarimovdima@ya.ru>
2022-09-15 17:35:33 +0300

    add permissions checks for access users with 'any' permission

--- src/main/java/com/bitniki/VPNconServer/controller/PeerController.java
+++ src/main/java/com/bitniki/VPNconServer/controller/PeerController.java
@@ -1,10 +1,8 @@
 package com.bitniki.VPNconServer.controller;
 
 import com.bitniki.VPNconServer.entity.PeerEntity;
-import com.bitniki.VPNconServer.exception.alreadyExistException.EntityAlreadyExistException;
 import com.bitniki.VPNconServer.exception.alreadyExistException.PeerAlreadyExistException;
 import com.bitniki.VPNconServer.exception.notFoundException.EntityNotFoundException;
-import com.bitniki.VPNconServer.exception.notFoundException.HostNotFoundException;
 import com.bitniki.VPNconServer.exception.notFoundException.PeerNotFoundException;
 import com.bitniki.VPNconServer.exception.notFoundException.UserNotFoundException;
 import com.bitniki.VPNconServer.exception.validationFailedException.PeerValidationFailedException;
@@ -12,6 +10,7 @@ import com.bitniki.VPNconServer.model.PeerWithAllRelations;
 import com.bitniki.VPNconServer.service.PeerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,31 +23,41 @@ public class PeerController {
 
 
     @GetMapping
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('peer:read')")
     public ResponseEntity<List<PeerWithAllRelations>> getAllPeers() {
         return ResponseEntity.ok(peerService.getAll());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('peer:read')")
     public ResponseEntity<PeerWithAllRelations> getOnePeer(@PathVariable Long id)
             throws PeerNotFoundException {
         return ResponseEntity.ok(peerService.getOne(id));
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('peer:write')")
     public ResponseEntity<PeerWithAllRelations> createPeer(@RequestParam Long user_id,
                                                            @RequestParam Long host_id,
                                                            @RequestBody PeerEntity peerEntity)
-            throws UserNotFoundException, HostNotFoundException, PeerAlreadyExistException, PeerValidationFailedException {
+            throws EntityNotFoundException, PeerAlreadyExistException, PeerValidationFailedException {
         return ResponseEntity.ok(peerService.create(user_id, host_id, peerEntity));
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('peer:write')")
     public ResponseEntity<PeerWithAllRelations> updatePeer(@PathVariable Long id, @RequestBody PeerEntity peer)
             throws PeerNotFoundException, PeerAlreadyExistException, PeerValidationFailedException {
         return ResponseEntity.ok(peerService.update(id, peer));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('peer:write')")
     public ResponseEntity<PeerWithAllRelations> deletePeer(@PathVariable Long id) throws PeerNotFoundException {
         return ResponseEntity.ok(peerService.delete(id));
     }
