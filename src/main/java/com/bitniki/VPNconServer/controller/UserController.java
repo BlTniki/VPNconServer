b8527241e6847b83e80b55e53eb434779e04cdb2
BITniki <shakarimovdima@ya.ru>
2022-09-15 13:04:11 +0300

    add permissions checks for access users with 'any' permission

--- src/main/java/com/bitniki/VPNconServer/controller/UserController.java
+++ src/main/java/com/bitniki/VPNconServer/controller/UserController.java
@@ -9,6 +9,7 @@ import com.bitniki.VPNconServer.model.UserWithRelations;
 import com.bitniki.VPNconServer.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,11 +21,15 @@ public class UserController {
     private UserService userService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('any')" +
+                    "&& hasAuthority('user:read')")
     public ResponseEntity<List<UserWithRelations>> getAllUsers() {
             return ResponseEntity.ok(userService.getAll());
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('user:read')")
     public ResponseEntity<UserWithRelations> getUser(@PathVariable Long id)
             throws UserNotFoundException {
         return ResponseEntity.ok(userService.getOne(id));
@@ -37,12 +42,16 @@ public class UserController {
     }
 
     @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('user:write')")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody UserEntity user)
             throws UserNotFoundException, UserAlreadyExistException, UserValidationFailedException {
         return ResponseEntity.ok(userService.update(id, user));
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('any')" +
+            "&& hasAuthority('user:write')")
     public ResponseEntity<User> deleteUser(@PathVariable Long id)
             throws UserNotFoundException {
         return ResponseEntity.ok(userService.delete(id));
