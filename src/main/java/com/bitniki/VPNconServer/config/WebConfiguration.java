238c7a745f29a73b1637cea78ec65e555f255b46
BITniki <shakarimovdima@ya.ru>
2022-12-09 16:46:48 +0300

    Making the definition of allowed originals in application.properties

--- src/main/java/com/bitniki/VPNconServer/config/WebConfiguration.java
+++ src/main/java/com/bitniki/VPNconServer/config/WebConfiguration.java
@@ -1,5 +1,6 @@
 package com.bitniki.VPNconServer.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,14 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    @Value("${cors.allowedOrigins}")
+    private String[] origins;
     //Allow request from any domain
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                //if you read this text you should know:
-                //Author so dump, so he even forgets
-                //to configure allowedOrigins properly
-                .allowedOrigins("*")
+                .allowedOrigins(origins)
                 .allowedMethods("*");
     }
 }
