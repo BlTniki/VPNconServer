e12445ca944232d690dfd320f10b4f2c656cabdc
BITniki <shakarimovdima@ya.ru>
2023-02-07 15:06:24 +0300

    added new find method

--- src/main/java/com/bitniki/VPNconServer/repository/UserRepo.java
+++ src/main/java/com/bitniki/VPNconServer/repository/UserRepo.java
@@ -3,9 +3,11 @@ package com.bitniki.VPNconServer.repository;
 import com.bitniki.VPNconServer.entity.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends CrudRepository<UserEntity, Long> {
     UserEntity findByLogin (String login);
     Optional<UserEntity> findByTelegramId(Long telegramId);
+    List<UserEntity> findBySubscriptionExpirationDayIsNotNull();
 }
