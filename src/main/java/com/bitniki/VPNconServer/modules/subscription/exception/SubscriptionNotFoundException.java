0e712da4c479d222bb7996e3ac4fc28379a1837e
BITniki <shakarimovdima@ya.ru>
2023-04-26 20:56:46 +0300

    deleting modules for now

--- src/main/java/com/bitniki/VPNconServer/modules/subscription/exception/SubscriptionNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bitniki.VPNconServer.modules.subscription.exception;
-
-import com.bitniki.VPNconServer.exception.EntityNotFoundException;
-
-public class SubscriptionNotFoundException extends EntityNotFoundException {
-    public SubscriptionNotFoundException(String message) {
-        super(message);
-    }
-}
