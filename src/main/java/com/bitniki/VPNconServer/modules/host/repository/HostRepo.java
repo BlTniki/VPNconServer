e6114b23738a64804bf1be3e6226f3fda2cf7241
BITniki <shakarimovdima@ya.ru>
2023-04-27 18:34:29 +0300

    added Optional

--- src/main/java/com/bitniki/VPNconServer/modules/host/repository/HostRepo.java
+++ src/main/java/com/bitniki/VPNconServer/modules/host/repository/HostRepo.java
@@ -3,6 +3,9 @@ package com.bitniki.VPNconServer.modules.host.repository;
 import com.bitniki.VPNconServer.modules.host.entity.HostEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface HostRepo extends CrudRepository<HostEntity, Long> {
-    HostEntity findByIpadress (String ipadress);
+    Optional<HostEntity> findByIpaddress(String ipaddress);
+    Optional<HostEntity> findByName(String name);
 }
