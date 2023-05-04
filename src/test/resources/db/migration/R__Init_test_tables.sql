1cfbc0472e7bd910eebd7b26b33dccb24e78c661
BITniki <shakarimovdima@ya.ru>
2023-05-04 22:47:53 +0300

    update

--- src/test/resources/db/migration/R__Init_test_tables.sql
+++ src/test/resources/db/migration/R__Init_test_tables.sql
@@ -1,7 +1,7 @@
 -- Use this file to insert test data
 -- ${flyway:timestamp}
 DELETE FROM users;
-INSERT INTO users
+INSERT INTO users (id, login, password, telegram_id, telegram_first_name, telegram_nickname)
     VALUES
-        (1, 'test', 'aA123456'),
-        (2, 'test2', 'aA123456');
+        (1, 'test', 'aA123456', 1, 'test', 'test'),
+        (2, 'test2', 'aA123456', 2, 'test2', 'test2');
