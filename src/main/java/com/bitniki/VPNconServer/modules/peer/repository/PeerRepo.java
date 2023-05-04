0a55b83591eda3ce2c57cc34bc4af1dd5ad57dfe
BITniki <shakarimovdima@ya.ru>
2023-05-04 20:17:57 +0300

    fix @Query

--- src/main/java/com/bitniki/VPNconServer/modules/peer/repository/PeerRepo.java
+++ src/main/java/com/bitniki/VPNconServer/modules/peer/repository/PeerRepo.java
@@ -8,14 +8,33 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PeerRepo extends CrudRepository<PeerEntity, Long> {
-    @Query("SELECT p FROM peers p WHERE p.peer_conf_name = :peerConfName AND p.user_id = :userId AND p.host_id = :hostId")
+    @Query(
+            value = "SELECT p FROM peers p WHERE p.peer_conf_name = :peerConfName AND p.user_id = :userId AND p.host_id = :hostId",
+            nativeQuery = true
+    )
     Optional<PeerEntity> findByPeerConfNameAndUserIdAndHostId(String peerConfName, Long userId, Long hostId);
-    @Query("SELECT p FROM peers p WHERE p.user.login = :login")
+
+    @Query(
+        value = "SELECT p FROM peers p INNER JOIN users u ON u.id = p.user_id WHERE u.login = :login",
+        nativeQuery = true
+    )
     Iterable<PeerEntity> findAllWithUserLogin(String login);
-    @Query("SELECT p FROM peers p WHERE p.id = :id AND p.user.login = :login")
+
+    @Query(
+            value = "SELECT p FROM peers p WHERE p.id = :id AND p.user.login = :login",
+            nativeQuery = true
+    )
     Optional<PeerEntity> findByIdAndUserLogin(Long id, String login);
-    @Query("SELECT p FROM peers p WHERE p.host_id = :hostId")
+
+    @Query(
+            value = "SELECT p FROM peers p WHERE p.host_id = :hostId",
+            nativeQuery = true
+    )
     List<PeerEntity> findAllByHostId(Long hostId);
-    @Query("SELECT p FROM peers p WHERE p.peer_ip = :peerIp AND p.host_id = :hostId")
+
+    @Query(
+            value = "SELECT p FROM peers p WHERE p.peer_ip = :peerIp AND p.host_id = :hostId",
+            nativeQuery = true
+    )
     Optional<PeerEntity> findByPeerIpAndHostId(String peerIp, Long hostId);
 }
