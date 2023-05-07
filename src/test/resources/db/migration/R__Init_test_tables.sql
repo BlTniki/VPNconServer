73fd273ffd9c05771ccb247bbe181c9a4ade8706
BITniki <shakarimovdima@ya.ru>
2023-05-07 14:23:23 +0300

    added host test records

--- src/test/resources/db/migration/R__Init_test_tables.sql
+++ src/test/resources/db/migration/R__Init_test_tables.sql
@@ -5,3 +5,9 @@ INSERT INTO users (id, login, password, telegram_id, telegram_first_name, telegr
     VALUES
         (1, 'test', 'aA123456', 1, 'test', 'test'),
         (2, 'test2', 'aA123456', 2, 'test2', 'test2');
+
+DELETE FROM hosts;
+INSERT INTO hosts (id, name, ipaddress, port, host_internal_network_prefix, host_password, host_public_key)
+    VALUES
+        (1, 'test', '127.0.0.1', 1, '127.0.0.0', '123456', 'lolkek'),
+        (2, 'test2', '127.0.0.1', 2, '127.0.0.0', '123456', 'lolkek');
