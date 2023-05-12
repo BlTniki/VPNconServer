ca2d7dbb3287548f467a1c41b84e8aecbabadd26
BITniki <shakarimovdima@ya.ru>
2023-05-12 18:06:43 +0300

    added new dependency for test purpose

--- src/main/java/com/bitniki/VPNconServer/modules/peer/connectHandler/PeerConnectHandlerService.java
+++ src/main/java/com/bitniki/VPNconServer/modules/peer/connectHandler/PeerConnectHandlerService.java
@@ -10,27 +10,55 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PeerConnectHandlerService {
+
+    /**
+     * Создать пира на хосте.
+     * @param peer сущность {@link PeerEntity}.
+     * @return Ответ хоста в виде {@link PeerFromHost} с заполненными private и public ключами.
+     * @throws PeerConnectHandlerException В случае проблем на стороне хоста.
+     */
     public PeerFromHost createPeerOnHost(PeerEntity peer) throws PeerConnectHandlerException {
         PeerConnectHandler peerConnectHandler = new PeerConnectHandler(peer);
         return peerConnectHandler.createPeerOnHost();
     }
 
+    /**
+     * Удаляет пира на хосте.
+     * @param peer сущность {@link PeerEntity}.
+     * @throws PeerConnectHandlerException В случае проблем на стороне хоста.
+     */
     public void deletePeerOnHost(PeerEntity peer) throws PeerConnectHandlerException {
         //deleting peer on host
         PeerConnectHandler peerConnectHandler = new PeerConnectHandler(peer);
         peerConnectHandler.deletePeerOnHost();
     }
 
+    /**
+     * Создаёт токен для скачивания конфига для туннеля с хоста.
+     * @param peer сущность {@link PeerEntity}.
+     * @return Токен в виде url-safe строки.
+     * @throws PeerConnectHandlerException В случае проблем на стороне хоста.
+     */
     public String getDownloadConfToken(PeerEntity peer) throws PeerConnectHandlerException {
         PeerConnectHandler peerConnectHandler = new PeerConnectHandler(peer);
         return peerConnectHandler.getDownloadConfToken();
     }
 
+    /**
+     * Активирует пира на хосте.
+     * @param peer сущность {@link PeerEntity}.
+     * @throws PeerConnectHandlerException В случае проблем на стороне хоста.
+     */
     public void activateOnHost(PeerEntity peer) throws PeerConnectHandlerException {
         PeerConnectHandler peerConnectHandler = new PeerConnectHandler(peer);
         peerConnectHandler.activateOnHost();
     }
 
+    /**
+     * Деактивирует пира на хосте.
+     * @param peer сущность {@link PeerEntity}.
+     * @throws PeerConnectHandlerException В случае проблем на стороне хоста.
+     */
     public void deactivateOnHost(PeerEntity peer) throws PeerConnectHandlerException {
         PeerConnectHandler peerConnectHandler = new PeerConnectHandler(peer);
         peerConnectHandler.deactivateOnHost();
