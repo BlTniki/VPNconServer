aedbb977eabf1f4c926a6fb12720dce005b48398
BITniki <shakarimovdima@ya.ru>
2023-07-20 17:18:45 +0300

    init metacode tests

---
+++ src/test/java/com/bitniki/VPNconServer/metacode/MetacodeServiceTest.java
@@ -0,0 +1,39 @@
+package com.bitniki.VPNconServer.metacode;
+
+import com.bitniki.VPNconServer.VpNconServerApplicationTests;
+import com.bitniki.VPNconServer.exception.EntityNotFoundException;
+import com.bitniki.VPNconServer.modules.metacodes.model.MetacodeToUse;
+import com.bitniki.VPNconServer.modules.metacodes.service.MetacodeService;
+import com.bitniki.VPNconServer.modules.user.entity.UserEntity;
+import com.bitniki.VPNconServer.modules.user.role.Role;
+import com.bitniki.VPNconServer.modules.user.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@Rollback
+public class MetacodeServiceTest extends VpNconServerApplicationTests {
+    @Autowired
+    private MetacodeService metacodeService;
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testResolvingCodes_Valid() throws EntityNotFoundException {
+        Long MODEL_USER_ID = 105L;
+        String ADMIN_CODE = "blabla2";
+
+        UserEntity user = userService.getOneById(MODEL_USER_ID);
+
+        MetacodeToUse metacodeToUse = MetacodeToUse.builder().code(ADMIN_CODE).build();
+        metacodeService.resolveMetacodeByLogin(user.getLogin(), metacodeToUse);
+
+        user = userService.getOneById(MODEL_USER_ID);
+
+        assertEquals(user.getRole(), Role.ADMIN);
+    }
+}
--- src/test/resources/db/migration/R__Init_test_tables.sql
+++ src/test/resources/db/migration/R__Init_test_tables.sql
@@ -6,7 +6,8 @@ INSERT INTO users (id, login, password, role, telegram_id, telegram_nickname)
         (101, 'telegramBot', '$2a$12$wZxtK/hdO9ylNN3BrvrB4uXPWF3JHukBEuTnbx2x60F62wvQTFnTO', 'ADMIN', null, null),
         (102, 'accountant', '$2a$12$OmT1v3R579qFuryy6Hms0e/DxjYP0Ad8Wi6lEdSpZRQ.GK2BVyREi', 'ADMIN', null, null),
         (103, 'test', 'aA123456', 'ACTIVATED_USER', 1, 'test'),
-        (104, 'test2', 'aA123456', 'ACTIVATED_USER', 2, 'test2');
+        (104, 'test2', 'aA123456', 'ACTIVATED_USER', 2, 'test2'),
+        (105, 'testMeta', 'aA123456', 'DEACTIVATED_USER', 2, 'testMeta'); -- user for metacodes test
 
 DELETE FROM hosts;
 INSERT INTO hosts (id, name, ipaddress, port, host_internal_network_prefix, host_password, host_public_key)
@@ -21,4 +22,11 @@ INSERT INTO peers (id, peer_conf_name, peer_ip, peer_private_key, peer_public_ke
         (101, 'test', '10.8.0.10', 'private', 'public', TRUE, 103, 101),
         (102, 'test2', '10.8.0.11', 'private', 'public', TRUE, 103, 101),
         (103, 'test3', '10.8.0.12', 'private', 'public', TRUE, 104, 101),
-        (104, 'test', '10.8.0.10', 'private', 'public', TRUE, 103, 102);
+        (104, 'test', '10.8.0.10', 'private', 'public', FALSE, 103, 102),
+        (105, 'test123', '10.8.0.14', 'private', 'public', TRUE, 103, 102);
+
+DELETE FROM metacodes;
+INSERT INTO metacodes (id, code, operation)
+    VALUES
+        (101, 'blabla1', 'UPDATE_ROLE_TO_ACTIVATED_CLOSE_USER'),
+        (102, 'blabla2', 'UPDATE_ROLE_TO_ADMIN');
