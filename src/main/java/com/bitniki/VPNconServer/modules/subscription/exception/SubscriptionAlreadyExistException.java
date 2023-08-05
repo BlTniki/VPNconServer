e24e5000bb614116d207862880b8563a2c7c67c6
BITniki <shakarimovdima@ya.ru>
2023-08-05 17:26:46 +0300

    removed redundant exception

--- src/main/java/com/bitniki/VPNconServer/modules/subscription/exception/SubscriptionAlreadyExistException.java
+++
@@ -1,9 +0,0 @@
-package com.bitniki.VPNconServer.modules.subscription.exception;
-
-import com.bitniki.VPNconServer.exception.EntityAlreadyExistException;
-
-public class SubscriptionAlreadyExistException extends EntityAlreadyExistException {
-    public SubscriptionAlreadyExistException(String message) {
-        super(message);
-    }
-}
