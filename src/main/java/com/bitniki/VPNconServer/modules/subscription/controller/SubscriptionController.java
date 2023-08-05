727b57f279a2d3ab798c58c2612ba189f77095ff
BITniki <shakarimovdima@ya.ru>
2023-08-05 17:26:03 +0300

    Added documentation comments

--- src/main/java/com/bitniki/VPNconServer/modules/subscription/controller/SubscriptionController.java
+++ src/main/java/com/bitniki/VPNconServer/modules/subscription/controller/SubscriptionController.java
@@ -1,7 +1,6 @@
 package com.bitniki.VPNconServer.modules.subscription.controller;
 
 import com.bitniki.VPNconServer.modules.role.Role;
-import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionAlreadyExistException;
 import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionNotFoundException;
 import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionValidationFailedException;
 import com.bitniki.VPNconServer.modules.subscription.model.Subscription;
@@ -21,6 +20,11 @@ public class SubscriptionController {
     @Autowired
     private SubscriptionService subscriptionService;
 
+    /**
+     * Получает список всех подписок.
+     *
+     * @return Ответ со списком подписок
+     */
     @GetMapping
     @PreAuthorize("hasAuthority('subscription:read')")
     public ResponseEntity<List<Subscription>> getAll() {
@@ -31,6 +35,12 @@ public class SubscriptionController {
         );
     }
 
+    /**
+     * Получает список подписок по указанной роли.
+     *
+     * @param role Роль, для которой ищутся подписки
+     * @return Ответ со списком подписок для указанной роли
+     */
     @GetMapping("/byRole/{role}")
     @PreAuthorize("hasAuthority('subscription:read')")
     public ResponseEntity<List<Subscription>> getByRole(@PathVariable Role role) {
@@ -41,6 +51,13 @@ public class SubscriptionController {
         );
     }
 
+    /**
+     * Получает информацию о подписке по указанному идентификатору.
+     *
+     * @param id Идентификатор подписки
+     * @return Ответ с информацией о подписке
+     * @throws SubscriptionNotFoundException если подписка не найдена
+     */
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('subscription:read')")
     public ResponseEntity<Subscription> getOne(@PathVariable Long id)
@@ -50,6 +67,13 @@ public class SubscriptionController {
         );
     }
 
+    /**
+     * Создает новую подписку.
+     *
+     * @param model Данные для создания подписки
+     * @return Ответ с информацией о созданной подписке
+     * @throws SubscriptionValidationFailedException если данные недостаточны или некорректны для создания подписки
+     */
     @PostMapping
     @PreAuthorize("hasAuthority('subscription:write')")
     public ResponseEntity<Subscription> create(@RequestBody SubscriptionFromRequest model)
@@ -59,15 +83,30 @@ public class SubscriptionController {
         );
     }
 
+    /**
+     * Обновляет информацию о существующей подписке.
+     *
+     * @param id    Идентификатор подписки, которую необходимо обновить
+     * @param model Новые данные для подписки
+     * @return Ответ с обновленной информацией о подписке
+     * @throws SubscriptionNotFoundException         если подписка не найдена
+     */
     @PutMapping("/{id}")
     @PreAuthorize("hasAuthority('subscription:write')")
     public ResponseEntity<Subscription> update(@PathVariable Long id, @RequestBody SubscriptionFromRequest model)
-            throws SubscriptionValidationFailedException, SubscriptionAlreadyExistException, SubscriptionNotFoundException {
+            throws SubscriptionNotFoundException {
         return ResponseEntity.ok(
                 Subscription.toModel(subscriptionService.updateById(id, model))
         );
     }
 
+    /**
+     * Удаляет подписку по указанному идентификатору.
+     *
+     * @param id Идентификатор подписки, которую необходимо удалить
+     * @return Ответ с информацией об удаленной подписке
+     * @throws SubscriptionNotFoundException если подписка не найдена
+     */
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('subscription:write')")
     public ResponseEntity<Subscription> deleteOne(@PathVariable Long id)
--- src/main/java/com/bitniki/VPNconServer/modules/subscription/service/SubscriptionService.java
+++ src/main/java/com/bitniki/VPNconServer/modules/subscription/service/SubscriptionService.java
@@ -2,7 +2,6 @@ package com.bitniki.VPNconServer.modules.subscription.service;
 
 import com.bitniki.VPNconServer.modules.role.Role;
 import com.bitniki.VPNconServer.modules.subscription.entity.SubscriptionEntity;
-import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionAlreadyExistException;
 import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionNotFoundException;
 import com.bitniki.VPNconServer.modules.subscription.exception.SubscriptionValidationFailedException;
 import com.bitniki.VPNconServer.modules.subscription.model.SubscriptionFromRequest;
@@ -12,15 +11,50 @@ import java.util.List;
 import java.util.Spliterator;
 
 public interface SubscriptionService {
+    /**
+     * Возвращает список всех подписок.
+     * @return {@link Spliterator}, содержащий объекты {@link SubscriptionEntity}.
+     */
     Spliterator<SubscriptionEntity> getAll();
 
+    /**
+     * Возвращает список всех подписок с определённой ролью.
+     * @param role Роль, по которой выполняется поиск.
+     * @return Список {@link SubscriptionEntity}.
+     */
     List<SubscriptionEntity> getAllByRole(@NotNull Role role);
 
+    /**
+     * Возвращает подписку с данным Id.
+     * @param id Id подписки.
+     * @return Сущность подписки.
+     * @throws SubscriptionNotFoundException Если подписка с данным Id не найдена.
+     */
     SubscriptionEntity getOneById(@NotNull Long id) throws SubscriptionNotFoundException;
-    
-    SubscriptionEntity create(@NotNull SubscriptionFromRequest model) throws SubscriptionValidationFailedException;
-    
-    SubscriptionEntity updateById(@NotNull Long id, @NotNull SubscriptionFromRequest model) throws SubscriptionNotFoundException, SubscriptionAlreadyExistException, SubscriptionValidationFailedException;
 
+    /**
+     * Создаёт новую подписку.
+     * @param model Модель {@link SubscriptionFromRequest} для создания подписки.
+     * @return Сущность созданной подписки.
+     * @throws SubscriptionValidationFailedException если валидация для модели не прошла успешно.
+     */
+    SubscriptionEntity create(@NotNull SubscriptionFromRequest model) throws SubscriptionValidationFailedException;
+
+    /**
+     * Обновление сущности подписки.
+     * @param id Id существующей подписки.
+     * @param model Модель {@link SubscriptionFromRequest} для обновления подписки.
+     *              Следует указать только те поля, которые необходимо обновить.
+     * @return Сущность обновлённой подписки.
+     * @throws SubscriptionNotFoundException Если подписка с данным Id не найдена.
+     */
+    SubscriptionEntity updateById(@NotNull Long id, @NotNull SubscriptionFromRequest model) throws SubscriptionNotFoundException;
+
+    /**
+     * Удаление подписки с данным Id.
+     * @param id  Id подписки.
+     * @return Сущность удалённой подписки.
+     * @throws SubscriptionNotFoundException Если подписка с данным Id не найдена.
+     */
     SubscriptionEntity deleteById(@NotNull Long id) throws SubscriptionNotFoundException;
 }
