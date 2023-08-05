528bd0c67ef268b6a888898c51d685e20dd53cf6
BITniki <shakarimovdima@ya.ru>
2023-08-05 21:34:40 +0300

    rename domain url

--- src/main/java/com/bitniki/VPNconServer/modules/subscription/controller/UserSubscriptionController.java
+++ src/main/java/com/bitniki/VPNconServer/modules/subscription/controller/UserSubscriptionController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.stream.StreamSupport;
 
 @RestController
-@RequestMapping("/user_subs")
+@RequestMapping("/userSubs")
 public class UserSubscriptionController {
     @Autowired
     private UserSubscriptionService userSubscriptionService;
