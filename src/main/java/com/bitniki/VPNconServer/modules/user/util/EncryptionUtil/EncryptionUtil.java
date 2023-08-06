fb3660c731bf2ba68543fb34d2f96551a90592fe
BITniki <shakarimovdima@ya.ru>
2023-08-06 22:40:06 +0300

    add doc comment block

--- src/main/java/com/bitniki/VPNconServer/modules/user/util/EncryptionUtil/EncryptionUtil.java
+++ src/main/java/com/bitniki/VPNconServer/modules/user/util/EncryptionUtil/EncryptionUtil.java
@@ -1,6 +1,22 @@
 package com.bitniki.VPNconServer.modules.user.util.EncryptionUtil;
 
+/**
+ * Утилита для кодирования и декодирования строки.
+ * Закодированная строка является URL безопасной.
+ */
 public interface EncryptionUtil {
+    /**
+     * Кодирование строки.
+     * Закодированная строка является URL безопасной.
+     * @param originalString Строка, которую следует закодировать.
+     * @return URL безопасная закодированная строка.
+     */
     String encode(String originalString);
+
+    /**
+     * Декодирование строки.
+     * @param encryptedString Строка, которую следует декодировать.
+     * @return декодированная строка.
+     */
     String decode(String encryptedString);
 }
