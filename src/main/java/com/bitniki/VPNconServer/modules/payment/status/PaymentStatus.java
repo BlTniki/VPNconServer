11081e3f952b83b1c8625af8fadd172343e13dab
BITniki <shakarimovdima@ya.ru>
2023-08-07 17:51:30 +0300

    add status page name

--- src/main/java/com/bitniki/VPNconServer/modules/payment/status/PaymentStatus.java
+++ src/main/java/com/bitniki/VPNconServer/modules/payment/status/PaymentStatus.java
@@ -1,7 +1,17 @@
 package com.bitniki.VPNconServer.modules.payment.status;
 
 public enum PaymentStatus {
-    IN_PROCESS,
-    COMPLETED,
-    FAILURE
+    IN_PROCESS("in_process"),
+    COMPLETED("completed"),
+    FAILURE("error");
+
+    private final String statusPageName;
+
+    PaymentStatus(String statusPageName) {
+        this.statusPageName = statusPageName;
+    }
+
+    public String getStatusPageName() {
+        return statusPageName;
+    }
 }
