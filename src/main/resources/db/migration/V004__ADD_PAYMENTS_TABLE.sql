ba582e6115db516021c877255c137b19d8b9a484
BITniki <shakarimovdima@ya.ru>
2023-08-07 17:52:06 +0300

    fix field type

--- src/main/resources/db/migration/V004__ADD_PAYMENTS_TABLE.sql
+++ src/main/resources/db/migration/V004__ADD_PAYMENTS_TABLE.sql
@@ -2,7 +2,7 @@ CREATE TABLE payments (
     uuid VARCHAR PRIMARY KEY,
     user_id BIGINT NOT NULL,
     subscription_id BIGINT NOT NULL,
-    to_pay MONEY NOT NULL,
+    to_pay VARCHAR NOT NULL,
     time_stamp TIMESTAMP NOT NULL,
     status VARCHAR NOT NULL
 );
