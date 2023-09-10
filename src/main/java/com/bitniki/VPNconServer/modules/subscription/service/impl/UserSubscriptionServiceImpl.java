1401ce74fbab0ecc566c69ddd7cb6166cc7c60fd
BITniki <shakarimovdima@ya.ru>
2023-09-10 22:30:43 +0300

    fix cron

--- src/main/java/com/bitniki/VPNconServer/modules/subscription/service/impl/UserSubscriptionServiceImpl.java
+++ src/main/java/com/bitniki/VPNconServer/modules/subscription/service/impl/UserSubscriptionServiceImpl.java
@@ -222,7 +222,7 @@ public class UserSubscriptionServiceImpl implements UserSubscriptionService {
     }
 
     @Override
-    @Scheduled(cron = "0 30 22 * * *")
+    @Scheduled(cron = "0 35 22 * * *")
     public void checkExpirationDay() throws EntityNotFoundException, ReminderValidationFailedException, PeerConnectHandlerException {
         final LocalDate TODAY = LocalDate.now();
         final String TODAY_REMINDER_TEXT = "Твоя подписка сгорела :(\n\nЕсли желаешь продолжить пользоваться сервисом, то стоит оплатить подписку";
