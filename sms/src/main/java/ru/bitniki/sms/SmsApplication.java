a8cbc969994bc935108c8a47f7e9a5d8331442fb
BITniki <shakarimovdima@ya.ru>
2024-08-18 15:13:49 +0300

    add config annotations to SmsApplication

--- sms/src/main/java/ru/bitniki/sms/SmsApplication.java
+++ sms/src/main/java/ru/bitniki/sms/SmsApplication.java
@@ -3,9 +3,13 @@ package ru.bitniki.sms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableR2dbcRepositories
+@EnableWebFlux
 public class SmsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SmsApplication.class, args);
