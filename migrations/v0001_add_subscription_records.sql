4f55a2c81777ea7a42ae7739bebd4c1445ef6199
BITniki <shakarimovdima@ya.ru>
2024-08-27 19:07:03 +0300

    add default subscription records

---
+++ migrations/v0001_add_subscription_records.sql
@@ -0,0 +1,6 @@
+INSERT INTO subscriptions (role, price_in_rubles, allowed_active_peers_count, duration) VALUES
+    ('ADMIN', 1, 1000, '10 years'),
+    ('ACTIVATED_CLOSE_USER', 50, 100, '1 months'),
+    ('ACTIVATED_USER', 70, 1, '1 month'),
+    ('ACTIVATED_USER', 100, 5, '1 month')
+;
